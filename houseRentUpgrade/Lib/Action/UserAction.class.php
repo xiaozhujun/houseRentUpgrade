71a6319c6d350d4fee9e7088fe7734397acc1a81
xiaozhujun <xiaozhujun520@163.com>
2013-09-04 21:20:58 +0800

    房源发布页及其它页面调整

--- houseRentUpgrade/Lib/Action/UserAction.class.php
+++ houseRentUpgrade/Lib/Action/UserAction.class.php
@@ -56,7 +56,6 @@ class UserAction extends Action {
 				}
 				// 发送邮件
 				$email_result = sendMail ( $email, $realName, $subject, $email_content );
-				echo $email_result;
 				
 				$data['msg'] =  '恭喜您，注册成功！已发送激活邮件到'.$email.',请您注意查收激活邮件！';
 				$data['success'] = true;
